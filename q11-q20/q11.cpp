66eb80e394de616e450412390c651fb8bbc9ea50
MidVC <lutony040720@gmail.com>
2023-08-22 20:18:59 +0800

    Added q11

---
+++ q11-q20/q11.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int area = (right - left) * min(height[left], height[right]);
+            maxArea = max(maxArea, area);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxArea;
+    }
+};
+
+int main() {
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    cout << Solution().maxArea(height) << endl;
+}
