9bdcee841782a3d29cdb2c41773d38c00b2b5daf
MidVC <lutony040720@gmail.com>
2025-09-21 19:24:26 +0000

    q48

---
+++ 2025-secondhalf/q48.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n/2; ++i) {
+            for (int j = i; j < n-i-1; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = tmp;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<vector<int>> matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+    // Solution s;
+    // vector<int> ans = s.spiralOrder(matrix);
+}
